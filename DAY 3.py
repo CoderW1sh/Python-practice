a26376ec42b9f88d98704c12de4031777cf2fc69
codecraftman <mohammadkaunenpathan@gmail.com>
2025-06-16 10:19:30 +0530

    Upload Day 1, Day 2, and Day 3 Python practice files
    Initial commit of daily practice Python files

---
+++ DAY 3.py
@@ -0,0 +1,47 @@
+"""x = float(input("Enter the number 1:"))
+y = float(input("Enter the number 2:"))
+z = x+y
+print(z)
+
+x = float(input("Enter the number 1:"))
+y = float(input("Enter the number 2:"))
+z = x*y
+print(z)
+
+x = float(input("Enter the number 1:"))
+y = float(input("Enter the number 2:"))
+z = x/y
+print(z)
+
+x = float(input("Enter the number 1:"))
+y = float(input("Enter the number 2:"))
+z = x-y
+print(z)
+
+x = float(input("Enter the number 1:"))
+y = float(input("Enter the number 2:"))
+z = x//y
+print(z)
+
+x = float(input("Enter the number 1:"))
+y = float(input("Enter the number 2:"))
+z = x%y
+print(z)  ("Mistake no 1. Write by own not knowing the x,y and in print i can print many functions ")"""
+
+
+x = int(input("Enter the number 1 :"))
+
+y = int(input("Enter the number 2 :"))
+
+print("The Sum :",x + y)
+print("The Difference :",x - y)
+print("The Product :",x * y)
+print("The Quentient (/) :",(x /y)*2)#Mistake no 2. Dont know how to find the quotient, (x/y)2 And You commented out correct line is print("The Quentient  :",(x /y)*2):
+# print("The Division :",x/y) and This line is actually correct if you're showing real division. You should uncomment it and use it with a clear .#
+print("The Quentient(//) :",x //y)
+#print("The Division :",x/y). Mistake no 3 Silly#
+print("The Remainder :", x%y)#Mistake no 4 dont know how to find the remainder#
+
+
+
+
