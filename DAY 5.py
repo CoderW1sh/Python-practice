63ac623309e24034eb7ce407c6f9df95b8e7ce12
codecraftman <mohammadkaunenpathan@gmail.com>
2025-06-18 15:25:24 +0530

    Add files via upload

---
+++ DAY 5.py
@@ -0,0 +1,12 @@
+x = float(input ("Enter  the Number 1:"))
+y = float(input ("Enter  the Number 2:"))
+
+if    x>y :
+        print(x*y)
+elif  x<y:
+        print(x/y)
+else:
+       print(x- y)
+
+       print("The Output is")
+       
