0f7e036460899b805a48fdd9cdc79201081e8e31
codecraftman <mohammadkaunenpathan@gmail.com>
2025-06-18 16:12:15 +0530

    Add files via upload

---
+++ Day 6.py
@@ -0,0 +1,33 @@
+#This is the first program#
+x = int(input("Enter the Number 1 ="))
+y = int(input("Enter the Number 2 ="))
+
+if x>y:
+          print(x*y)
+elif x <y :
+            print(x/y)
+else:
+     print(x-y)
+print("The Output  is")
+
+
+
+#This is  the second program#
+
+x = int(input("Enter the no1:"))
+u = int(input("Enter the no 2:"))
+
+print("The Sum :", x +u)
+print("The Difference :",x-u)
+print("The Remainder :",x%u)#Here the M.1 is that is using this sign (/) in remainder#
+print("The Division :",x/u)
+
+
+print("The Data type is ",type(x))
+
+
+
+
+
+
+
