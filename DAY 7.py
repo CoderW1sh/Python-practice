926b56785af441e4503c62bcf2a2a2d22f2a10d0
codecraftman <mohammadkaunenpathan@gmail.com>
2025-06-19 16:47:34 +0530

    Add files via upload

---
+++ DAY 7.py
@@ -0,0 +1,10 @@
+name = input("Enter the Name:")
+age = int(input("Enter the Age:"))
+m =  int(input("Enter the marks of subject 1:"))
+a = int(input("Enter the marks of subject 2:"))# Mistake is i am not using the int #
+r = int(input("Enter the marks of subject 3 :"))
+k = m +a +r
+print("Total marks:", k)
+Average   = k/3 
+print("Average  marks :" , Average)
+print("Hello",name,"Your average marks are",Average)
